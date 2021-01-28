18f8585165a4b94ef9b186e2953d7b8016d4d66d
Yury Alifanov <yury.alifanov@dataart.com>
2021-01-28 16:33:03 +0200

    Added Settings file

---
+++ ToolsQA1/Framework/Settings/Settings.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+
+namespace ToolsQA1.Framework.Settings
+{
+    public static class Settings
+    {
+        public static string BrowserType { get; }
+
+        public static string BaseUrl { get; }
+
+        static Settings()
+        {
+            BrowserType = ConfigurationManager.AppSettings["BrowserType"];
+            BaseUrl = ConfigurationManager.AppSettings["BaseUrl"];
+        }
+    }
+}
